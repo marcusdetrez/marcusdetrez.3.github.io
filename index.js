2c195d26ad3b127d8c83f2c653fba3759b305d74
marcusdetrez <marcus.detrez@gmail.com>
2018-11-16 00:35:43 +0100

    ajout de tout

---
+++ index.js
@@ -0,0 +1,10 @@
+function countPopup() {
+	setTimeout("Popup()",10000);
+	return false;
+}
+
+function Popup () {
+	if (window.confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) { 
+  window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}}
+
